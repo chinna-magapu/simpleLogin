877a5c5875cdc370bc8ad74b5ebc292286a23231
tmagapu <tatarao.magap@jnet.com>
2020-01-28 17:54:10 +0530

    update

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -10,8 +10,18 @@ export class Login extends React.Component<any, any>{
         this.props.setAllUsers();
     }
     render() {
-        return <div>
-            Helloo Login.
-        </div>
+        return <form method="post" action="index.html">
+            <div className="box">
+                <h1>Dashboard</h1>
+
+                <input type="email" name="email" value="" className="email" />
+
+                <input type="password" name="email" value="" className="email" />
+
+                <a href="#"><div className="btn">Sign In</div></a>
+
+                <a href="#"><div id="btn2">Sign Up</div></a>
+            </div>
+        </form>
     }
 }
